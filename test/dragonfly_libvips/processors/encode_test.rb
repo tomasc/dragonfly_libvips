12cf52f200be604af013befebf3ed249dbf25d7d
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-27 22:02:45 +0200

    improve output

--- test/dragonfly_libvips/processors/encode_test.rb
+++ test/dragonfly_libvips/processors/encode_test.rb
@@ -5,28 +5,32 @@ describe DragonflyLibvips::Processors::Encode do
   let(:content_image) { Dragonfly::Content.new(app, SAMPLES_DIR.join('sample.png')) } # 280x355
   let(:processor) { DragonflyLibvips::Processors::Encode.new }
 
-  DragonflyLibvips::SUPPORTED_FORMATS.each do |format|
-    unless File.exists?(SAMPLES_DIR.join("sample.#{format}"))
-      it(format) { skip "sample.#{format} does not exist, skipping" }
-      next
-    end
+  describe 'SUPPORTED_FORMATS' do
+    DragonflyLibvips::SUPPORTED_FORMATS.each do |format|
+      unless File.exist?(SAMPLES_DIR.join("sample.#{format}"))
+        it(format) { skip "sample.#{format} does not exist, skipping" }
+        next
+      end
 
-    describe format.to_s do
       let(:content) { app.fetch_file SAMPLES_DIR.join("sample.#{format}") }
-      let(:result) { content.encode('jpg') }
-      it { content.encode('jpg').ext.must_equal 'jpg' }
-      it { content.encode('jpg').mime_type.must_equal 'image/jpeg' }
-      it { content.encode('jpg').size.must_be :>, 0 }
+      it(format) do
+        result = content.encode('jpg')
+        content.encode('jpg').ext.must_equal 'jpg'
+        content.encode('jpg').mime_type.must_equal 'image/jpeg'
+        content.encode('jpg').size.must_be :>, 0
+      end
     end
   end
 
-  DragonflyLibvips::SUPPORTED_OUTPUT_FORMATS.each do |format|
-    describe "output to #{format}" do
+  describe 'SUPPORTED_OUTPUT_FORMATS' do
+    DragonflyLibvips::SUPPORTED_OUTPUT_FORMATS.each do |format|
       let(:content) { app.fetch_file SAMPLES_DIR.join("sample.png") }
-      let(:result) { content.encode(format) }
-      it { result.ext.must_equal format }
-      it { result.mime_type.must_equal Rack::Mime.mime_type(".#{format}") }
-      it { result.size.must_be :>, 0 }
+      it(format) do
+        result = content.encode(format)
+        result.ext.must_equal format
+        result.mime_type.must_equal Rack::Mime.mime_type(".#{format}")
+        result.size.must_be :>, 0
+      end
     end
   end
 
