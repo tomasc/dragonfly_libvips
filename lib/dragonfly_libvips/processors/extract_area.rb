13e95bcddd5fc7b3aa0ddd47ae4909fa11a3b694
bartuz <filip.bartuzi@gmail.com>
2022-09-08 14:26:20 +0200

    Update all DragonflyLibvips.symbolize_keys to new syntax

--- lib/dragonfly_libvips/processors/extract_area.rb
+++ lib/dragonfly_libvips/processors/extract_area.rb
@@ -28,11 +28,11 @@ module DragonflyLibvips
         output_options.delete("Q") unless /jpg|jpeg/i.match?(format.to_s)
         output_options["format"] ||= format.to_s if /gif|bmp/i.match?(format.to_s)
 
-        img = ::Vips::Image.new_from_file(content.path, DragonflyLibvips.symbolize_keys(input_options))
+        img = ::Vips::Image.new_from_file(content.path, **DragonflyLibvips.symbolize_keys(input_options))
         img = img.extract_area(x, y, width, height)
 
         content.update(
-          img.write_to_buffer(".#{format}", DragonflyLibvips.symbolize_keys(output_options)),
+          img.write_to_buffer(".#{format}", **DragonflyLibvips.symbolize_keys(output_options)),
           "name" => "temp.#{format}",
           "format" => format
         )
